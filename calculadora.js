1954b347a8a1cd7551131e1858116295fb942c7c
Laura Maria <162618856+laur4mp@users.noreply.github.com>
2024-05-13 16:36:34 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,46 @@
+
+function calcularSoma (){
+    let num1 = document.getElementById('num1').value 
+    let num2 = document.getElementById('num2').value
+    let resultado = ''
+    !isNaN(num1) && !isNaN(num2)? resultado = num1+num2:alert('Insira um número válido')
+    let resposta = document.getElementById('resposta')
+    resposta.textContent = resultado
+    console.log(resultado)
+}
+function calcularMultiplicacao(){
+    let num1 = document.getElementById('num1').value 
+    let num2 = document.getElementById('num2').value
+    let resultado = ''
+    !isNaN(num1) && !isNaN(num2)?resultado = num1*num2:alert('Insira um número válido')
+    let resposta = document.getElementById('resposta')
+    resposta.textContent = resultado
+    
+}
+function calcularSubtracao(){
+    let num1 = document.getElementById('num1').value 
+    let num2 = document.getElementById('num2').value
+    let resultado = ''
+    !isNaN(num1) && !isNaN(num2)?resultado = num1-num2:alert('Insira um número válido')
+    let resposta = document.getElementById('resposta')
+    resposta.textContent = resultado
+}
+function calcularDivisao(){
+    let num1 = document.getElementById('num1').value 
+    let num2 = document.getElementById('num2').value
+    let resultado = ''
+    !isNaN(num1) && !isNaN(num2)?resultado = num1/num2:alert('Insira um número válido')
+    let resposta = document.getElementById('resposta')
+    resposta.textContent = resultado;
+}
+let bSubi = document.getElementById('subtrairButao')
+bSubi.addEventListener('click', calcularSubtracao)
+
+let bSoma = document.getElementById('addButao')
+bSoma.addEventListener('click', calcularSoma)
+
+let bDivi = document.getElementById('dividirButao')
+bDivi.addEventListener('click', calcularDivisao)
+
+let bMulti = document.getElementById('multipicarButao')
+bMulti.addEventListener('click', calcularMultiplicacao)
